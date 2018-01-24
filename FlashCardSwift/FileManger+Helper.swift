89016674102eedd59ae99ec52b380a0a30db6e54
William Gromme <wgromme@yahoo.com>
2018-01-24 14:15:11 -0500

    added helper class for saving files

---
+++ FlashCardSwift/FileManger+Helper.swift
@@ -0,0 +1,20 @@
+//
+//  FileManger+Helper.swift
+//  SwiftCard
+//
+//  Created by Scott Gromme on 1/23/18.
+//  Copyright © 2018 Billys Awesome App House. All rights reserved.
+//
+
+import Foundation
+
+public extension FileManager {
+    static var documentDirectoryURL: URL {
+        return try! FileManager.default.url(
+            for: .userDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false)
+    }
+    
+}
